4e8cd6c138dd44dffc5625b664ccd437c61f66c0
Tiago Moreira <tsm_projetos@hotmail.com>
2020-01-05 14:32:32 -0300

    adicionado modificações da pasta projeto versão2-TesteSlider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /*
 $('.slider-principal').slick();
-  */  
+  */
 
-  $('.slider-principal').slick({
-    dots: true,//legenda do slider que informa a quantidade de itens e a seleção atual
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1, //quantidade de slider mostrados por vez
-    adaptiveHeight: true,
-    autoplay: true
-  });
+$('').slick({
+  dots: true,//legenda do slider que informa a quantidade de itens e a seleção atual
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1, //quantidade de slider mostrados por vez
+  adaptiveHeight: true,
+  autoplay: false
+});
