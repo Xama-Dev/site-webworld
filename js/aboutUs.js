4a6ea576b0f3cdab5444f1c637d480dd1beb3c60
Tiago Moreira <tsm_projetos@hotmail.com>
2020-02-05 15:57:46 -0300

    Versão 13.7: AboutUs page, funcionalidade changeBoxTestimonial. Aplicado designer pattern OOP.

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,191 +1,108 @@
-/*colori pictures no onmouseover secao 3 box de depoimentos*/
-var picture1Off = true;
-var picture2Off = true;
-var picture3Off = false;
-var picture4Off = true;
-var picture5Off = true;
-
-var picture1 = document.getElementById("picture1");
-var picture2 = document.getElementById("picture2");
-var picture3 = document.getElementById("picture3");
-var picture4 = document.getElementById("picture4");
-var picture5 = document.getElementById("picture5");
+const pictures = document.querySelectorAll('.pictures-section3')
+const boxTestimonials = document.querySelectorAll('.box-testimonial')
+let pointerEmployee = document.getElementById('pictures-section3')
+const boxTestimonialColection = []
 
 
+const boxTestimonialPrototype = {
+    picture: {},
+    boxTestimonial: {},
+    positionPointerEmployee: null,
+    active: function () {
+        this.picture.classList.remove('picture-no-active')
+        this.picture.classList.add('picture-active')
 
+        this.boxTestimonial.classList.remove('box-testimonial-no-active')
 
-picture1.onmouseover = function () {
-    picture1.style.cursor = "pointer";
-    if (picture1Off) {
-        picture1.style.filter = "none";
-    }
-}
-picture1.onmouseout = function () {
-    if (picture1Off) {
-        picture1.style.filter = "grayscale(100%)";
-    }
-}
-picture2.onmouseover = function () {
-    picture2.style.cursor = "pointer";
-    if (picture2Off) {
-        picture2.style.filter = "none";
-    }
-}
-picture2.onmouseout = function () {
-    if (picture2Off) {
-        picture2.style.filter = "grayscale(100%)";
+    },
+    desable: function () {
+        this.picture.classList.add('picture-no-active')
+        this.picture.classList.remove('picture-active')
+
+        this.boxTestimonial.classList.add('box-testimonial-no-active')
     }
 }
 
-picture3.onmouseover = function () {
-    picture3.style.cursor = "pointer";
-    if (picture3Off) {
-        picture3.style.filter = "none";
+const boxTestimonial1 = Object.create(boxTestimonialPrototype)
+boxTestimonial1.picture = pictures[0]
+boxTestimonial1.boxTestimonial = boxTestimonials[0]
+boxTestimonial1.pointerEmployee = '25%'
+
+const boxTestimonial2 = Object.create(boxTestimonialPrototype)
+boxTestimonial2.picture = pictures[1]
+boxTestimonial2.boxTestimonial = boxTestimonials[1]
+boxTestimonial2.pointerEmployee = '37%'
+
+const boxTestimonial3 = Object.create(boxTestimonialPrototype)
+boxTestimonial3.picture = pictures[2]
+boxTestimonial3.boxTestimonial = boxTestimonials[2]
+boxTestimonial3.pointerEmployee = '49%'
+
+const boxTestimonial4 = Object.create(boxTestimonialPrototype)
+boxTestimonial4.picture = pictures[3]
+boxTestimonial4.boxTestimonial = boxTestimonials[3]
+boxTestimonial4.pointerEmployee = '60%'
+
+const boxTestimonial5 = Object.create(boxTestimonialPrototype)
+boxTestimonial5.picture = pictures[4]
+boxTestimonial5.boxTestimonial = boxTestimonials[4]
+boxTestimonial5.pointerEmployee = '72%'
+
+
+function changeBoxTestimonial (picture) {
+    if (picture.id === 'picture1'){
+        boxTestimonial1.active()
+        boxTestimonial2.desable()
+        boxTestimonial3.desable()
+        boxTestimonial4.desable()
+        boxTestimonial5.desable() 
+        document.getElementById('pictures-section3').style.marginLeft = boxTestimonial1.pointerEmployee;
+
+    }else if (picture.id === 'picture2'){
+        boxTestimonial1.desable()
+        boxTestimonial2.active()
+        boxTestimonial3.desable()
+        boxTestimonial4.desable()
+        boxTestimonial5.desable()
+        document.getElementById('pictures-section3').style.marginLeft = boxTestimonial2.pointerEmployee;
+
+    }else if (picture.id === 'picture3'){
+        boxTestimonial1.desable()
+        boxTestimonial2.desable()
+        boxTestimonial3.active()
+        boxTestimonial4.desable()
+        boxTestimonial5.desable()
+        document.getElementById('pictures-section3').style.marginLeft = boxTestimonial3.pointerEmployee;
+
+    }else if (picture.id === 'picture4'){
+        boxTestimonial1.desable()
+        boxTestimonial2.desable()
+        boxTestimonial3.desable()
+        boxTestimonial4.active()
+        boxTestimonial5.desable()
+        document.getElementById('pictures-section3').style.marginLeft = boxTestimonial4.pointerEmployee;
+        
+    }else if (picture.id === 'picture5'){
+        boxTestimonial1.desable()
+        boxTestimonial2.desable()
+        boxTestimonial3.desable()
+        boxTestimonial4.desable()
+        boxTestimonial5.active()
+        document.getElementById('pictures-section3').style.marginLeft = boxTestimonial5.pointerEmployee;
+        
     }
 }
-picture3.onmouseout = function () {
-    if (picture3Off) {
-        picture3.style.filter = "grayscale(100%)";
+
+pictures.forEach((picture) =>{
+    picture.onmouseover = function () {
+        picture.classList.remove('picture-no-active')
+    }
+    picture.onmouseout = function () {
+        picture.classList.add('picture-no-active')
     }
 
-}
-picture4.onmouseover = function () {
-    picture4.style.cursor = "pointer";
-    if (picture4Off) {
-        picture4.style.filter = "none";
-    }
-}
-picture4.onmouseout = function () {
-    if (picture4Off) {
-        picture4.style.filter = "grayscale(100%)";
-    }
-}
-picture5.onmouseover = function () {
-    picture5.style.cursor = "pointer";
-    if (picture5Off) {
-        picture5.style.filter = "none";
-    }
-}
-picture5.onmouseout = function () {
-    if (picture5Off) {
-        picture5.style.filter = "grayscale(100%)";
-    }
-}
+    picture.addEventListener('click', function(){
+        changeBoxTestimonial(picture)
+    })
 
-
-
-/*Troca box de depoimento secao 3*/
-var boxTestimonial1 = document.getElementById("box-testimonial1");
-var boxTestimonial2 = document.getElementById("box-testimonial2");
-var boxTestimonial3 = document.getElementById("box-testimonial3");
-var boxTestimonial4 = document.getElementById("box-testimonial4");
-var boxTestimonial5 = document.getElementById("box-testimonial5");
-
-picture1.onclick = function () {
-    picture1Off = false;
-    picture2Off = true;
-    picture3Off = true;
-    picture4Off = true;
-    picture5Off = true;
-
-    boxTestimonial1.style.display = "block";
-    boxTestimonial2.style.display = "none";
-    boxTestimonial3.style.display = "none";
-    boxTestimonial4.style.display = "none";
-    boxTestimonial5.style.display = "none";
-
-    picture1.style.filter = "none";
-    picture2.style.filter = "grayscale(100%)";
-    picture3.style.filter = "grayscale(100%)";
-    picture4.style.filter = "grayscale(100%)";
-    picture5.style.filter = "grayscale(100%)";
-
-    document.getElementById("pictures-section3").style.marginLeft = "25%";
-}
-
-picture2.onclick = function () {
-    picture1Off = true;
-    picture2Off = false;
-    picture3Off = true;
-    picture4Off = true;
-    picture5Off = true;
-
-    boxTestimonial1.style.display = "none";
-    boxTestimonial2.style.display = "block";
-    boxTestimonial3.style.display = "none";
-    boxTestimonial4.style.display = "none";
-    boxTestimonial5.style.display = "none";
-
-    picture1.style.filter = "grayscale(100%)";
-    picture2.style.filter = "none";
-    picture3.style.filter = "grayscale(100%)";
-    picture4.style.filter = "grayscale(100%)";
-    picture5.style.filter = "grayscale(100%)";
-
-    document.getElementById("pictures-section3").style.marginLeft = "37%";
-}
-
-picture3.onclick = function () {
-    picture1Off = true;
-    picture2Off = true;
-    picture3Off = false;
-    picture4Off = true;
-    picture5Off = true;
-
-    boxTestimonial1.style.display = "none";
-    boxTestimonial2.style.display = "none";
-    boxTestimonial3.style.display = "block";
-    boxTestimonial4.style.display = "none";
-    boxTestimonial5.style.display = "none";
-
-    picture1.style.filter = "grayscale(100%)";
-    picture2.style.filter = "grayscale(100%)";
-    picture3.style.filter = "none";
-    picture4.style.filter = "grayscale(100%)";
-    picture5.style.filter = "grayscale(100%)";
-    document.getElementById("pictures-section3").style.marginLeft = "49%";
-}
-
-picture4.onclick = function () {
-    picture1Off = true;
-    picture2Off = true;
-    picture3Off = true;
-    picture4Off = false;
-    picture5Off = true;
-
-    boxTestimonial1.style.display = "none";
-    boxTestimonial2.style.display = "none";
-    boxTestimonial3.style.display = "none";
-    boxTestimonial4.style.display = "block";
-    boxTestimonial5.style.display = "none";
-
-    picture1.style.filter = "grayscale(100%)";
-    picture2.style.filter = "grayscale(100%)";
-    picture3.style.filter = "grayscale(100%)";
-    picture4.style.filter = "none";
-    picture5.style.filter = "grayscale(100%)";
-
-    document.getElementById("pictures-section3").style.marginLeft = "60%";
-}
-
-picture5.onclick = function () {
-    picture1Off = true;
-    picture2Off = true;
-    picture3Off = true;
-    picture4Off = true;
-    picture5Off = false;
-
-    boxTestimonial1.style.display = "none";
-    boxTestimonial2.style.display = "none";
-    boxTestimonial3.style.display = "none";
-    boxTestimonial4.style.display = "none";
-    boxTestimonial5.style.display = "block";
-
-    picture1.style.filter = "grayscale(100%)";
-    picture2.style.filter = "grayscale(100%)";
-    picture3.style.filter = "grayscale(100%)";
-    picture4.style.filter = "grayscale(100%)";
-    picture5.style.filter = "none";
-
-    document.getElementById("pictures-section3").style.marginLeft = "72%";
-}
+})
